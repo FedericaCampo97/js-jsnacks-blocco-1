a0804270cf8dcebaca8b5c5379c6eb8a15f2b4a6
FedericaCampo97 <federica.campo97@gmail.com>
2023-08-29 14:43:45 +0200

    Ask the user for the numbers

---
+++ jsnack1/main.js
@@ -0,0 +1,15 @@
+/*L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.*/
+
+//prompt x2
+//if/else
+//console.log
+
+//Chiedo il primo numero all'utente
+const firstUserNumber = prompt ('Scegli un numero');
+console.log (firstUserNumber);
+
+//Chiedo un secondo numero all'utente
+const secondUserNumber = prompt ('Scegli ancora un numero diverso dal primo');
+console.log (secondUserNumber);
+
