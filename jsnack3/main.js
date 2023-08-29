b760132d684749631bab037ea31cb7b241ed26b8
FedericaCampo97 <federica.campo97@gmail.com>
2023-08-29 20:03:03 +0200

    Snack3 complete

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,28 +1,23 @@
 /*Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.*/
 
-//prompt x 10
+//for
+  //prompt
+  //console.log
 
 
-//Chiedo all'utente il primo numero
-const firstNumber = prompt('scegli il primo numero');
-//Chiedo all'utente il secondo numero
-const secondNumber = prompt('scegli il secondo numero');
-//Chiedo all'utente il terzo numero 
-const thirdNumber = prompt('scegli il terzo numero');
-//Chiedo all'utente il quarto numero 
-const fourthNumber = prompt('scegli il quarto numero');
-//Chiedo all'utente il quinto numero 
-const fifthNumber = prompt('scegli il quinto numero');
-//Chiedo all'utente il sesto numero 
-const sixthNumber = prompt('scegli il sesto numero');
-//Chiedo all'utente il settimo numero 
-const seventhNumber = prompt('scegli il settimo numero');
-//Chiedo all'utente l'ottavo numero
-const eighthNumber = prompt('scegli ottavo numero');
-//Chiedo all'utente il nono numero 
-const ninthNumber = prompt('scegli il nono numero');
-//Chiedo all'utente il decimo numero
-const tenthNumber = prompt('scegli il decimo numero');
+let total = 0
+//inizio il ciclo
+for (let i = 0; i < 10; i++) {
+    //Chiedo 10 volte il numero
+    let number = prompt ('Scegli un numero');
+    //trasformo la stringa in numero
+    number = parseInt(number);
+    console.log(number);
+    //sommo ogni totale con il nuovo numero inserito dall'utente
+    total = number + total
+    //stampo in console il totale
+    console.log(total)
+}
 
 
