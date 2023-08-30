2dc20b75157f4cc848fa33c93828268c5698e6a7
FedericaCampo97 <federica.campo97@gmail.com>
2023-08-30 10:57:43 +0200

    while snack1

--- jsnack1/main.js
+++ jsnack1/main.js
@@ -6,18 +6,33 @@ Il software stampa il maggiore.*/
 //console.log
 
 //Chiedo il primo numero all'utente
-const firstUserNumber = prompt ('Scegli un numero');
+//const firstUserNumber = prompt ('Scegli un numero');
 
 
 //Chiedo un secondo numero all'utente
-const secondUserNumber = prompt ('Scegli ancora un numero diverso dal primo');
+//const secondUserNumber = prompt ('Scegli ancora un numero diverso dal primo');
 
 //Dichiaro e stampo sia in console che in pagina solo il numero maggiore 
-if (firstUserNumber > secondUserNumber) {
-    console.log(firstUserNumber)
-    document.getElementById ('result').innerHTML = ' Il numero vincente è ' + firstUserNumber
-} else if (firstUserNumber < secondUserNumber) {
-    console.log(secondUserNumber)
-    document.getElementById ('result').innerHTML = ' Il numero vincente è ' + secondUserNumber
+//if (firstUserNumber > secondUserNumber) {
+    //console.log(firstUserNumber)
+    //document.getElementById ('result').innerHTML = ' Il numero vincente è ' + firstUserNumber
+//} else if (firstUserNumber < secondUserNumber) {
+    //console.log(secondUserNumber)
+    //document.getElementById ('result').innerHTML = ' Il numero vincente è ' + secondUserNumber
+//}
+
+/*Da fare con WHILE tutti I prossimi snacks
+Snack 1
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.*/
+
+let x=1
+while (x <= 2) {
+    const number = prompt ('scegli un numero') 
+    x++
+    
 }
+
+
+
 
